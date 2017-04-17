8f8ba25e3b1032f2b2c77e00105c7f2b32a4b28f
Dunitt Monagas <dunitt.monagas@example.com>
2017-04-17 01:57:45 -0400

    Correción de errores y agregado nuevo método (Naive Bayes).

--- Proyecto.R
+++ Proyecto.R
@@ -1,8 +1,9 @@
-##Queremos saber el sexo de una persona de acuerdo a las propiedades acústicas de su voz
-#install.packages("caret")
-#install.packages("class")
+# Queremos saber el sexo de una persona de acuerdo a las propiedades acústicas de su voz
+# install.packages("caret")
+# install.packages("class")
 setwd("~/Escritorio/VoiceGender")
 library(shiny)
+library(rpart.plot)
 library(rmarkdown)
 library(class)
 library(randomForest)
@@ -12,41 +13,97 @@ library(e1071)
 library(rpart)
 library(tree)
 library(RWeka)
+library(party)
 library(C50)
 
-##Cargamos la data:
-raw = read.csv(file="voice.csv", header=TRUE)
-data = raw
-##[,c("meanfreq","sd","median","skew","kurt","sp.ent","sfm","mode","centroid","meanfun","meandom","dfrange","modindx","gender")]
-## KNN funciona mejor con:
-##data=raw[,c("IQR","sd","sp.ent","gender")]
+# Cargamos la data:
+raw <- read.csv(file = "voice.csv", header = TRUE)
+data <- raw
+# [,c("meanfreq","sd","median","skew","kurt","sp.ent","sfm","mode","centroid","meanfun","meandom","dfrange","modindx","gender")]
+# KNN funciona mejor con:
+# data<-raw[,c("IQR","sd","sp.ent","gender")]
 
-##Particionamos el dataset: 70% para entrenamiento y 30% para pruebas (esto se puede modificar para probar la precisión)
-particion = createDataPartition(y=data$gender, p=0.7, list=FALSE, times=1)
+# Particionamos el dataset: 70% para entrenamiento y 30% para pruebas (esto se puede modificar para probar la precisión)
+particion <- createDataPartition(y = data$gender, p = 0.7, list = FALSE, times = 1)
 dataTraining <- data[particion,]
 dataTest <- data[-particion,]
 dataTestF <- dataTest
-dataTestF$gender = NULL
+dataTestF$gender <- NULL
 
-##Utilizamos como número de vecinos K=3 para el criterio del algoritmo.
+# Utilizamos como número de vecinos K=3 para el criterio del algoritmo.
 
-cl=dataTraining$gender
-knnTraining = dataTraining
-knnTraining$gender=NULL
-kvecinos = knn(knnTraining, dataTestF, cl, k = 3)
-A1 = confusionMatrix(table(kvecinos,dataTest$gender))
-A1$overall[1]
+cl <- dataTraining$gender
+knnTraining <- dataTraining
+knnTraining$gender <- NULL
+# En general con l = 3 funciona mejor, pero deja muchos casos sin clasificar.
+kvecinos <- knn(knnTraining, dataTestF, cl, k = 3, l = 0)
+A1 <- confusionMatrix(table(kvecinos, dataTest$gender))
+# A1$overall[1]
+print("KNN [l = 3]")
+print(A1$overall[1])
 
-arbol <- J48(gender~.,dataTraining,control=Weka_control(C = 0.25, M=2))
-A2 = confusionMatrix(table(predict(arbol,dataTestF,type="class"), dataTest$gender))
-A2$overall[1]
+# -C Umbral de confianza para podar. (0.25)
+# -M Mínimo de instancias por hoja. (2)
+# No tiene mucho sentido asignar a la variable "control"
+# los valores que ya se usan por defecto.
+arbol <- J48(gender~., dataTraining, control = Weka_control(C = 0.25, M = 2))
+# Innecesario porque J48 ya genera una matriz de confusión.
+# A2 <- confusionMatrix(table(predict(arbol, dataTestF, type = "class"), dataTest$gender))
+# A2$overall[1]
+print("ÁRBOL")
+# print(A2$overall)
+# print(arbol)
+print(summary(arbol))
 
-tree <- randomForest(gender~.,dataTraining, ntree=20,norm.votes=FALSE)
-A3 = confusionMatrix(table(predict(tree,dataTestF,type="class"), dataTest$gender))
-A3$overall[1]
+# Para generar una imagen del árbol resultante.
+# if (require("party", quietly = TRUE)){
+#   plot(arbol)
+# }
 
-svm.model <- svm(gender~.,dataTraining,cost=62.5,gamma=0.5)
-A4= confusionMatrix(table(predict(svm.model,dataTestF,type="class"),dataTest$gender))
-A4$overall[1]
 
-##Conclusión: Usaremos Random Forest
+# Desconozco la utilidad de "importance" y "proximity".
+tree <- randomForest(gender~., dataTraining, importance = TRUE, proximity = TRUE,  ntree = 20, norm.votes = FALSE)
+A3 <- confusionMatrix(table(predict(tree, dataTestF, type = "class"), dataTest$gender))
+# A3$overall[1]
+print("BOSQUE ALEATORIO")
+print(tree$confusion)
+print(A3$overall[1])
+# Prefiero calcular la precisión de esta manera porque no tiene sentido usar métodos
+# externos cuando el paquete te ofrece la matriz de confusión (tree$confusion).
+mt <- as.data.frame(tree$confusion)
+mt[,"class.error"] <- NULL
+mt <- as.matrix(mt)
+print(sum(diag(mt))/sum(mt))
+
+
+
+# No se observa una diferencia significativa entre los tipos de kernel,
+# excepto por "sigmoid" que degrada notablemente la precisión.
+# (Con "radial" se obtiene el mejor resultado)
+# degree parameter needed for kernel of type polynomial (default: 3)
+# gamma parameter needed for all kernels except linear (default: 1/(data dimension))
+# coef0 parameter needed for kernels of type polynomial and sigmoid (default: 0)
+# kernel:
+# linear: u'v
+# polynomial: (γu'v + coef0)^degree
+# radial basis: (e − γ|u − v|^2)
+# sigmoid: tanh(γu'v + coef0)
+svm.model <- svm(gender~., dataTraining, kernel = "radial", cost = 62.5, gamma = 0.5, coef0 = 3, degree = 3)
+A4 <- confusionMatrix(table(predict(svm.model, dataTestF, type = "class"), dataTest$gender))
+#A4$overall[1]
+print("SVM")
+print(A4$overall[1])
+
+
+
+# Nuevo método agregado, si quieres dedicar tiempo a probar los parámetros sería genial.
+# Desconozco la utilidad del parámetro "laplace", pero afecta notablemente la precisión.
+naiveBayes.model <- naiveBayes(gender~., dataTraining, laplace = 4)
+A5 <- confusionMatrix(table(predict(naiveBayes.model, dataTestF, type = "class"), dataTest$gender))
+print("NAIVE BAYES")
+print(A5$overall[1])
+
+
+
+# Conclusión: Usaremos Random Forest
+# Yo concluí que J48 era ligeramente mejor.
